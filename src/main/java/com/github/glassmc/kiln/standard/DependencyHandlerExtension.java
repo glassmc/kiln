3d86118d6c575cdc7ce04f08423311cfc9c1455d
danterusdev <danterusdev@gmail.com>
2021-07-17 20:30:29 -0700

    shard dependencies in dev env should be working!

--- src/main/java/com/github/glassmc/kiln/standard/DependencyHandlerExtension.java
+++ src/main/java/com/github/glassmc/kiln/standard/DependencyHandlerExtension.java
@@ -5,11 +5,20 @@ import com.github.glassmc.kiln.common.Util;
 import com.github.glassmc.kiln.standard.mappings.NoSuchMappingsException;
 import com.github.glassmc.kiln.standard.mappings.ObfuscatedMappingsProvider;
 import com.github.glassmc.kiln.standard.mappings.YarnMappingsProvider;
+import com.github.jezza.Toml;
+import com.github.jezza.TomlTable;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.gradle.api.file.FileCollection;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+@SuppressWarnings("unused")
 public class DependencyHandlerExtension {
 
     public static FileCollection minecraft(String id, String version, String mappingsProviderId) {
@@ -57,8 +66,32 @@ public class DependencyHandlerExtension {
     public static FileCollection shard(String id, String version) {
         KilnStandardPlugin plugin = KilnStandardPlugin.getInstance();
 
-        File file = new File(plugin.getProject().getGradle().getGradleUserHomeDir() + "/caches/kiln/shard/" + id + "/" + version + "/" + id + "-" + version + ".jar");
-        return plugin.getProject().files(file.getAbsoluteFile());
+        File file = new File(plugin.getProject().getGradle().getGradleUserHomeDir() + "/caches/kiln/shard/" + id + "-" + version + ".jar");
+        file.getParentFile().mkdirs();
+
+        FileCollection files = plugin.getProject().files(file);
+
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/glassmc/registry/main/shards/" + id + "/" + version + "/" + id + "-" + version + ".toml");
+            String shardVersionData = IOUtils.toString(url, StandardCharsets.UTF_8);
+            TomlTable shardVersionDataTOML = Toml.from(new StringReader(shardVersionData));
+            if(!file.exists()) {
+                String shardFile = (String) shardVersionDataTOML.get("file");
+                if(shardFile != null) {
+                    FileUtils.copyURLToFile(new URL(shardFile), file);
+                }
+            }
+
+            TomlTable downloadsToml = (TomlTable) shardVersionDataTOML.getOrDefault("downloads", new TomlTable());
+            for(String downloadID : downloadsToml.keySet()) {
+                files = files.plus(shard(downloadID, (String) downloadsToml.get(downloadID)));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return files;
     }
 
 }
