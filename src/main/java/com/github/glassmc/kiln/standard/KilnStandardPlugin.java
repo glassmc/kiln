f7548bbc9f95f3cc7f03349550c5dbc13230dbe0
TheOddGarlic <umutinanerdogan62@gmail.com>
2021-09-02 17:35:51 +0300

    fix class file path oob part 3: I don't know java

--- src/main/java/com/github/glassmc/kiln/standard/KilnStandardPlugin.java
+++ src/main/java/com/github/glassmc/kiln/standard/KilnStandardPlugin.java
@@ -104,7 +104,7 @@ public class KilnStandardPlugin implements Plugin<Project> {
                     classReader.accept(classNode, 0);
 
                     String language = file.getAbsolutePath();
-                    language = language.replaceAll("\\", "/"); // Change path seperator for windows
+                    language = language.replace("\\", "/"); // Change path seperator for windows
                     language = language.substring(language.indexOf("classes/") + 8);
                     language = language.substring(0, language.indexOf("/"));
 
