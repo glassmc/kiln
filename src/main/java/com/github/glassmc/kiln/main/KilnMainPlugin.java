715f72141ec8f898ec7fb3e5502ea61c09b4f16d
danterusdev <danterusdev@gmail.com>
2021-09-05 18:43:01 -0700

    fix issue with multi-project builds

--- src/main/java/com/github/glassmc/kiln/main/KilnMainPlugin.java
+++ src/main/java/com/github/glassmc/kiln/main/KilnMainPlugin.java
@@ -4,18 +4,28 @@ import com.github.glassmc.kiln.main.task.GenerateRunConfiguration;
 import com.github.glassmc.kiln.main.task.GetRunConfiguration;
 import com.github.glassmc.kiln.standard.KilnStandardPlugin;
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
+import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 
-public class KilnMainPlugin extends KilnStandardPlugin {
+import java.io.File;
+
+public class KilnMainPlugin implements Plugin<Project> {
+
+    protected static KilnMainPlugin instance;
 
     public static KilnMainPlugin getInstance() {
-        return (KilnMainPlugin) instance;
+        return instance;
     }
+
+    protected Project project;
 
     @Override
     public void apply(Project project) {
-        super.apply(project);
+        instance = this;
+        this.project = project;
+
+        project.getPlugins().apply("kiln-standard");
 
         project.getTasks().register("getRunConfiguration", GetRunConfiguration.class);
         project.getTasks().register("genRunConfiguration", GenerateRunConfiguration.class);
@@ -30,6 +40,10 @@ public class KilnMainPlugin extends KilnStandardPlugin {
 
         ShadowJar shadowJar = (ShadowJar) project.getTasks().getByName("shadowJar");
         shadowJar.getConfigurations().add(project.getConfigurations().getByName("shadowRuntime"));
+    }
+
+    public File getCache() {
+        return KilnStandardPlugin.getInstance().getCache();
     }
 
 }
